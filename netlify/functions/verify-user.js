25e3d2468aae41e3bbdbd2d1000d9c98e65e3398
Zander Cheng <wafflestraws@icloud.com>
2025-08-19 19:30:33 +1000

    Test without check

--- netlify/functions/verify-user.js
+++ netlify/functions/verify-user.js
@@ -21,9 +21,9 @@ export async function handler(event) {
 
     //if (decoded.email.endsWith('@oakhill.nsw.edu.au')) { CHANGE TO ADD ! IN PRODUCTION
 
-    if (JSON.parse(process.env.WHITELISTED_USERS).includes(decoded.email)) {
-      return { statusCode: 403, body: 'Access denied' };
-    }
+    //if (JSON.parse(process.env.WHITELISTED_USERS).includes(decoded.email)) {
+    //  return { statusCode: 403, body: 'Access denied' };
+    //}
 
     return { statusCode: 200, body: JSON.stringify({ message: 'Access granted', whitelist: JSON.parse(process.env.WHITELISTED_USERS)}) };
   } catch (err) {
